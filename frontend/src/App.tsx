4c39e787c2ba184afec6535b981b6e06a6dd9feb
amauri323 <adsa_sk8@hotmail.com>
2021-11-07 16:59:58 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import DataTable from "componts/DataTable";
+import Footer from "componts/footer";
+import NavBar from "componts/NavBar";
+
 
 function App() {
   return (
-    <div>
-        <h2 className="text-primary">ola cris</h2>      
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h2 className="text-primary">ola cris</h2>
+        <DataTable />
+      </div>
+      <Footer />
+
+    </>
   );
 }
 
