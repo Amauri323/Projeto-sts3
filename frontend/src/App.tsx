e411937f140200dd12218bd7183c7a896fbfd821
amauri323 <adsa_sk8@hotmail.com>
2021-11-07 18:54:19 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "componts/Barchart";
 import DataTable from "componts/DataTable";
+import DonutChart from "componts/DonutChart";
 import Footer from "componts/footer";
 import NavBar from "componts/NavBar";
 
@@ -8,7 +10,20 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h2 className="text-primary">ola cris</h2>
+        <h2 className="text-primary py-3">Dashboard de Vendas</h2>
+        <div className="row px-3"> 
+          <div className="col-sm-6">
+            <h5 className="text-center text-secundary">Taxas de sucesso(%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secundary">Todas Vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
         <DataTable />
       </div>
       <Footer />
